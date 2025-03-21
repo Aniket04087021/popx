165bdb5a77c9e86d6817ee3848d834c567493757
ANIKET GUPTA <139427075+Aniket04087021@users.noreply.github.com>
2025-03-21 20:56:36 +0530

    Update api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+  baseURL:"https://popx-g3vt.onrender.com"
 });
 
 // Add token to requests
