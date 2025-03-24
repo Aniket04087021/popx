6cd06a831256731fe8f8fa511af79aa6bf863eae
ANIKET GUPTA <139427075+Aniket04087021@users.noreply.github.com>
2025-03-24 20:18:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <div className="app-container">
           <Routes>
-           
+            <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route 
